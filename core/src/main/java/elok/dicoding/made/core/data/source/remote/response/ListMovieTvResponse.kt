90d2968d8e72d2b65c797fc8d1e5af1f7fd61a0b
elokanugrah <33139266+elokanugrah@users.noreply.github.com>
2021-01-09 21:30:22 +0700

    [delete]: unused variable

--- core/src/main/java/elok/dicoding/made/core/data/source/remote/response/ListMovieTvResponse.kt
+++ core/src/main/java/elok/dicoding/made/core/data/source/remote/response/ListMovieTvResponse.kt
@@ -3,15 +3,6 @@ package elok.dicoding.made.core.data.source.remote.response
 import com.google.gson.annotations.SerializedName
 
 data class ListMovieTvResponse(
-    @field:SerializedName("page")
-    val page: Int?,
-
-    @field:SerializedName("total_results")
-    val total_results: Int?,
-
-    @field:SerializedName("total_pages")
-    val total_pages: Int?,
-
     @field:SerializedName("results")
     val results: List<MovieTvResponse>?
 )
